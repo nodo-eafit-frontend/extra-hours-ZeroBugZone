af95b6ec44f4c5ab6a3895c1c3fe0627cae23cde
Vero-nica22 <mcveronica66@gmail.com>
2024-08-10 19:24:11 -0500

    Modificación de archivos data

--- extra-hours/server/apis/get_empleados.js
+++ extra-hours/server/apis/get_empleados.js
@@ -1,20 +1,3 @@
-/*require('dotenv').config();
-const { readJsonFile } = require('../utils/json-reader');
-// const gameInfoJSON = require('../data/game-info.json');
-
-const EmpleadoInfo = async (request, response) => {
-	try {
-		const empleadoInfoJSON = await readJsonFile(process.env.JSON_Empleados_INFO); 
-        		response.status(200).send(empleadoInfoJSON);
-	} catch (error) {
-		response.status(400);
-	}
-};
-
-module.exports = {
-	getEmpleadoInfo,
-};
-*/
 require('dotenv').config();
 const { readJsonFile } = require('../utils/json-reader');
 
