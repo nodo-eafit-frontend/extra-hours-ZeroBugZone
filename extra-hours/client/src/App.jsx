a772c29b3c96490995b5129ed7e2d455265cf3b4
Andrés Felipe Toro González <pipetoro19@hotmail.com>
2024-08-12 21:40:08 -0500

    "Add new files and modify some files to the project"

--- extra-hours/client/src/App.jsx
+++ extra-hours/client/src/App.jsx
@@ -1,38 +1,35 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 import BasicButtons from "./components/Boton";
-import { InputNumber } from 'antd';
-
-  
-
+import { InputNumber } from "antd";
+import Brand from "./components/Brand";
 
 function App() {
   const [count, setCount] = useState(0);
   const [id, setId] = useState(1);
   const [horasExtras, setHoras] = useState(0);
-  const onChange = value => {
+  const onChange = (value) => {
     setId(value);
   };
   useEffect(() => {
-
     fetch("http://localhost:4000/extra-hours/" + id)
       .then((res) => res.json())
       .then((data) => {
         let horas = 0;
-        data.forEach(element => {
+        data.forEach((element) => {
           horas += element.horasExtras;
         });
         setHoras(horas);
       });
   }, [id]);
 
-
-
   return (
     <>
-      <div>
-      <InputNumber min={1} max={10} defaultValue={3} onChange={onChange} />;
+        <Brand />
+      
 
+      <div>
+        <InputNumber min={1} max={10} defaultValue={3} onChange={onChange} />;
       </div>
       <h1>Vite + React</h1>
       <div className="card">
@@ -44,7 +41,9 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        {horasExtras ?"las Horas extras son"+ horasExtras : "No hay horas extras"}
+        {horasExtras
+          ? "las Horas extras son" + horasExtras
+          : "No hay horas extras"}
       </p>
       <BasicButtons />
     </>
---
+++ extra-hours/client/src/components/Brand.jsx
@@ -0,0 +1,10 @@
+function Brand() {
+    return (
+      <header>
+        <div  className="brand" style={{color: '#eff0f8', fontSize: 25, fontFamily: 'Libre Baskerville', fontWeight: '700', wordWrap: 'break-word'}}>TEST ONE</div>
+      </header>
+     
+    );
+  }
+  
+  export default Brand;
