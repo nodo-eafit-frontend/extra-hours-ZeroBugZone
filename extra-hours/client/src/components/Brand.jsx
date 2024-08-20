b5446929782995b5d52b3f7344ade4d1f67e5413
Andrés Felipe Toro González <pipetoro19@hotmail.com>
2024-08-20 18:57:19 -0500

    Add rutas absolutas

--- extra-hours/client/src/components/Brand.jsx
+++ extra-hours/client/src/components/Brand.jsx
@@ -1,18 +1,23 @@
-import settingsIcon from '../assets/icons/settings.png';
-import notificationsIcon from '../assets/icons/Notifications.png';
+import settingsIcon from '@assets/icons/settings.png';
+import notificationsIcon from '@assets/icons/Notifications.png';
 
 function Brand() {
   return (
     <header>
-      <div className="brand" style={{ color: 'rgb(52, 60, 106)', fontSize: 25, fontFamily: 'Libre Baskerville', fontWeight: '700', wordWrap: 'break-word' }}>
-        AMADEUS
-        <span className="brand_span" style={{ color: '#2B3674', fontSize: 25, fontFamily: 'Libre Baskerville', fontWeight: '700', wordWrap: 'break-word' }}>Análisis Horas Extras</span>
-        <img src={settingsIcon} alt="Configuración de la página" />
-        <img src={notificationsIcon} alt="Notificaciones" />
+      <div className="brand" style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', padding: '0 20px' }}>
+        <div style={{ color: 'rgb(52, 60, 106)', fontSize: 25, fontFamily: 'Libre Baskerville', fontWeight: '700' }}>
+          AMADEUS
+        </div>
+        <div style={{ color: '#2B3674', fontSize: 25, fontFamily: 'Libre Baskerville', fontWeight: '700', textAlign: 'center', flex: 1 }}>
+          Análisis Horas Extras
+        </div>
+        <div style={{ display: 'flex', gap: '10px' }}>
+          <img src={settingsIcon} alt="Configuración de la página" />
+          <img src={notificationsIcon} alt="Notificaciones" />
+        </div>
       </div>
     </header>
   );
 }
 
 export default Brand;
-
--- extra-hours/client/vite.config.js
+++ extra-hours/client/vite.config.js
@@ -4,5 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
-//Falta  cogurar alias
+  "compilerOptions": {
+    "baseUrl": "src",
+    "paths": {
+      "@components/*": ["components/*"],
+      "@assets/*": ["assets/*"],
+      "@utils/*": ["utils/*"]
+    }
+  },
+  "include": ["src"],
+});
